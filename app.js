327837de0c6f0072a9598cfc1e77c4dc745c48d4
pinndot <ropup@naver.com>
2024-09-03 17:34:05 +0900

    #3.1

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
-const calculator = {
-    add: function (a, b) {
-        return a + b;
-    },
-    minus: function (a, b) {
-        return a - b;
-    },
-    muliply: function (a, b) {
-        return a * b;
-    },
-    divide: function (a, b) {
-        return a / b;
-    },
-    powerof: function (a, b) {
-        return a ** b;
-    },
-};
+const title = document.getElementById("title");
 
-console.log(calculator.add(8, 2));
-calculator.minus(8, 2);
-calculator.muliply(8, 2);
-calculator.divide(8, 2);
-calculator.powerof(8, 2);
+title.innerText = "Got you!";
+
+console.dir(title);
