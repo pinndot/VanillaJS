1c0f8beb0311cd3daac2c6ad4c1ccd631c62be7a
pinndot <ropup@naver.com>
2024-09-05 10:54:40 +0900

    #5.0

--- app.js
+++
@@ -1,8 +0,0 @@
-const h1 = document.querySelector("div.hello:first-child h1");
-// const h1 = document.querySelector("div.hello:last-child h1");
-
-function handleTitleClick() {
-    h1.classList.toggle("active");
-}
-
-h1.addEventListener("click", handleTitleClick);
---
+++ js/greetings.js
@@ -0,0 +1,30 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+
+const greeting = document.querySelector("#greeting");
+
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+    event.preventDefault(); // 기본적으로 하는 동작을 막음(Refresh를 막기 위해 사용)
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    loginForm.classList.add("hidden");
+    paintGreeting(username);
+}
+
+function paintGreeting(username) {
+    greeting.innerText = `Hello! ${username}.`;
+    greeting.classList.remove("hidden");
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove("hidden");
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    const username = localStorage.getItem(USERNAME_KEY);
+    paintGreeting(username);
+}
